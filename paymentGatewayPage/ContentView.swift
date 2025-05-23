538259a81ae2c029514df724c76302b6c19d3389
Aadish Jain <aadishhere@gmail.com>
2025-05-23 11:25:13 +0530

    Initial Commit

---
+++ paymentGatewayPage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  paymentGatewayPage
+//
+//  Created by Aadish Jain on 23/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ paymentGatewayPage/paymentGatewayPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  paymentGatewayPageApp.swift
+//  paymentGatewayPage
+//
+//  Created by Aadish Jain on 23/05/25.
+//
+
+import SwiftUI
+
+@main
+struct paymentGatewayPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
